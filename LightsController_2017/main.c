e1a1698a4ec0827948b18f21dcd7ef0aee2022ec
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-18 15:09:38 +0100

    Added Atmel Studio project and UM drivers submodule

---
+++ LightsController_2017/main.c
@@ -0,0 +1,18 @@
+/*
+ * LightsController_2017.c
+ *
+ * Created: 18/03/17 15:05:46
+ * Author : Sondre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
