c21debb9caf1c0210f63bf67dfc9d6a45452e542
jorgeja <mrjackwitz@gmail.com>
2017-05-23 17:55:02 +0200

    using modified timer and pwm drivers, working brakelights, not yet working blinkers

--- LightsController_2017/main.c
+++ LightsController_2017/main.c
@@ -1,26 +1,66 @@
 /*
- * LightsController_2017.c
+ * TestProject.c
  *
- * Created: 11/04/17 13:53:21
- *  Author: Sondre
+ * Created: 12.04.2017 11:22:52
+ * Author : Ultrawack
  */ 
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include "UniversalModuleDrivers/can.h"
-#include "UniversalModuleDrivers/rgbled.h"
-#include "UniversalModuleDrivers/timer.h"
+#include "UniversalModuleDrivers/usbdb.h"
+#include "UniversalModuleDrivers/rgbled.h" 
 #include "UniversalModuleDrivers/pwm.h"
+#include "UniversalModuleDrivers/can.h"
+#include "UniversalModuleDrivers/timer.h"
 
+#define BR PWM_PE3
+#define FR PWM_PE4
+#define BL PWM_PE5
+#define FL PWM_PB4	
+
+typedef enum {
+	FRONT,
+	REAR
+} LightModule_t;
+
+typedef enum {
+	OFF = 0,
+	NORMAL,
+	BRAKE
+} LightMode_t;
+
+typedef enum {
+	NONE = 0,
+	LEFT,
+	RIGHT,
+	HAZARD	
+} LightBlinkMode_t;
+
+#define BLINK_COUNT 4
+#define LIGHTS_ON_BIT		0b00000001
+#define HAZARD_BIT			0b00000100
+#define LEFT_BLINKER_BIT	0b00001000
+#define RIGHT_BLINKER_BIT	0b00010000
+
+
+//DEFINE THE PARAMETERS HERE
+uint8_t blink_period = 250;	
+LightBlinkMode_t BLINK_STATE = NONE;
+LightMode_t LIGHT_STATE = OFF;
+LightModule_t LIGHT_MODULE = REAR;
+uint8_t light_level = 0;
+uint8_t blink_decrement = BLINK_COUNT;
 CanMessage_t rxFrame;
-CanMessage_t txFrame;
 
-uint8_t lightlvl = 0;
+void handle_can();
+void blink_left();
+void blink_right();
+void hazards();
 
 int main(void)
 {
-	can_init(0, 0);
+	can_init(0,0);
 	rgbled_init();
 	timer_init();
 	pwm_init();
@@ -29,90 +69,212 @@ int main(void)
 	pwm_set_top_t3(0xFF);
 	sei();
 	
-	timer_start(TIMER3);
-	
-	DDRE |= 1<<PE3; //Initiate (output) PE3, Front Right
-	DDRE |= 1<<PE4; //Initiate (output) PE4, Front Left
-	DDRE |= 1<<PE5; //Initiate (output) PE5, Front Right blinker
-	DDRB |= 1<<PB4; //Initiate (output) PB4, Front Left blinker
-	
-	PORTE &= ~(1 << PE3);
-	PORTE &= ~(1 << PE4);
-	PORTE &= ~(1 << PE5);
-	PORTB &= ~(1 << PB4);
-	
-	int count_right;
-	int count_left;
-	
-	while (1) {
-		if (can_read_message_if_new(&rxFrame)) {
-			rgbled_toggle(LED_ALL);
+	while(1)
+	{
+		handle_can();
+		switch(LIGHT_STATE)
+		{
+			case OFF:
+			rgbled_turn_on(LED_RED);
+			pwm_set_duty_cycle(BR,0x00);
+			pwm_set_duty_cycle(BL,0x00);
+			pwm_set_duty_cycle(FR,0x00);
+			pwm_set_duty_cycle(FL,0x00);
+			break;
 			
-			if (rxFrame.id == DASHBOARD_CAN_ID) {
-				if (rxFrame.data.u8[0] & (1<<0)) {
-					pwm_set_duty_cycle(PWM_PE3,0xFF);
-					pwm_set_duty_cycle(PWM_PE4,0xFF);
-							
-					} else {
-					pwm_set_duty_cycle(PWM_PE3,0);
-					pwm_set_duty_cycle(PWM_PE4,0);
-					}
-				lightlvl = rxFrame.data.u8[1];
-				
-			} else if (rxFrame.id == STEERING_WHEEL_CAN_ID) {
-				if (rxFrame.data.u8[1] & (1 << 4)) {
-					count_left = 0;
-					count_right = 1;
-				}
-				if (rxFrame.data.u8[1] & (1 << 3)) {
-					count_right = 0;
-					count_left = 1;
-				}
-				
-				if (((timer_elapsed_ms(TIMER3) >= 500) && (rxFrame.data.u8[1] & (1 << 4))) || ((timer_elapsed_ms(TIMER3)>= 500) && count_right > 0)) {
-					if (count_right > 0 && count_right <= 6) {
-						pwm_set_duty_cycle(PWM_PB4,0xFF);
-						count_left = 0;
-						pwm_set_duty_cycle(PWM_PE5,0);
-						count_right++;
-					} 
-					else if (count_right > 6) {
-						pwm_set_duty_cycle(PWM_PB4,0xFF);
-						count_left = 0;
-						pwm_set_duty_cycle(PWM_PB4,0xFF);
-						count_right = 0;
-					} 
-					else {
-						pwm_set_duty_cycle(PWM_PB4,0xFF);
-						count_left = 0;
-						pwm_set_duty_cycle(PWM_PE5,0);
-					}
-				}
-				
-				if (((timer_elapsed_ms(TIMER3) >= 500) && (rxFrame.data.u8[1] & (1 << 3))) || ((timer_elapsed_ms(TIMER3)>= 500) && count_left > 0)) {
-					if (count_left > 0 && count_left <= 6) {
-						PORTE &= ~(1 << PE5);
-						count_right = 0;
-						PORTB ^= (1 << PB4);
-						count_left++;
-						} else if (count_left > 6) {
-						PORTB &= ~(1 << PB4);
-						count_left = 0;
-						PORTB &= ~(1 << PB4);
-						count_left = 0;
-						} else {
-						PORTE &= ~(1 << PE5);
-						count_right = 0;
-						PORTB ^= (1<< PB4);
-					}
-				}
-				
-				if (timer_elapsed_ms(TIMER3) >= 500) {
-					timer_start(TIMER3);
-				}
+			case BRAKE:
+			if (LIGHT_MODULE == REAR)
+			{
+				pwm_set_duty_cycle(FL,0xFF);
+				pwm_set_duty_cycle(FR,0x0F);
 			}
+			case NORMAL:
+			if ((LIGHT_STATE != BRAKE) && (LIGHT_MODULE == REAR)){
+				pwm_set_duty_cycle(FL,0x0F);
+				pwm_set_duty_cycle(FR,0xF0);
+			} else if (LIGHT_MODULE == FRONT){
+				pwm_set_duty_cycle(FL,light_level);
+				pwm_set_duty_cycle(FR,light_level);
+			}
+			
+			switch (BLINK_STATE)
+			{
+				case LEFT:
+				blink_left();
+				break;	
+				case RIGHT:
+				blink_right();
+				break;
+				case HAZARD:
+				hazards();
+				break;
+				default:
+				break;
+			}
+			break;
+			default:
+			break;
 		}
-		_delay_ms(1);
 	}
 }
 
+void handle_can()
+ {
+	 if(can_read_message_if_new(&rxFrame))
+	 {	
+		rgbled_toggle(LED_BLUE);
+		if (rxFrame.id == BRAKE_CAN_ID)
+		 {
+			 if (LIGHT_STATE != OFF)
+			 {
+				 if (rxFrame.data.u8[0])
+				 {
+					 rgbled_toggle(LED_RED);
+					 LIGHT_STATE = BRAKE;
+				 } 
+				 else if (!(rxFrame.data.u8[0]) && (LIGHT_STATE == BRAKE))
+				 {
+					 rgbled_turn_off(LED_RED);
+					 LIGHT_STATE = NORMAL;
+				 }
+			 }
+		 }
+		 if (rxFrame.id == DASHBOARD_CAN_ID)
+		 {
+			 if (rxFrame.data.u8[0] & LIGHTS_ON_BIT && LIGHT_STATE == OFF)
+			 {
+				rgbled_turn_off(LED_RED);
+				rgbled_turn_on(LED_GREEN);
+				LIGHT_STATE = NORMAL;
+			 } 
+			 else if (!(rxFrame.data.u8[0] & LIGHTS_ON_BIT) && LIGHT_STATE != OFF)
+			 {
+				rgbled_turn_off(LED_GREEN);
+				rgbled_turn_on(LED_RED);
+				LIGHT_STATE = OFF;		
+			 }
+			 
+			 if (rxFrame.data.u8[1] & HAZARD_BIT)
+			 {
+				 pwm_set_duty_cycle(BR,0x00);
+				 pwm_set_duty_cycle(BL,0x00);
+				 BLINK_STATE = HAZARD;
+			 }
+			 else if (!(rxFrame.data.u8[1] & HAZARD_BIT) && (BLINK_STATE == HAZARD))
+			 {
+				 BLINK_STATE = NONE;
+			 }
+			 
+			 light_level = rxFrame.data.u8[1];
+		 }
+		 
+		 if (rxFrame.id == STEERING_WHEEL_CAN_ID)
+		 {
+			if (rxFrame.data.u8[1] & LEFT_BLINKER_BIT) 
+			{
+				rgbled_turn_on(LED_RED);
+				if ((BLINK_STATE == NONE) | (BLINK_STATE == LEFT))
+				{
+					BLINK_STATE = LEFT;
+					blink_decrement = BLINK_COUNT;
+				} 
+				else if (BLINK_STATE == RIGHT)
+				{
+					pwm_set_duty_cycle(BR,0x00);
+					BLINK_STATE = LEFT;
+					blink_decrement = BLINK_COUNT;
+				}
+			} 
+			else if (rxFrame.data.u8[1] & RIGHT_BLINKER_BIT)
+			{
+				if ((BLINK_STATE == NONE) | (BLINK_STATE == LEFT))
+				{
+					BLINK_STATE = RIGHT;
+					blink_decrement = BLINK_COUNT;
+				} 
+				else if (BLINK_STATE == RIGHT)
+				{
+					pwm_set_duty_cycle(BL,0x00);
+					BLINK_STATE = RIGHT;
+					blink_decrement = BLINK_COUNT;
+				}
+			}
+		}
+	}
+ }
+ 
+ void blink_left()
+ {
+	 if (blink_decrement)
+	 {
+		 if (!timer_is_running(TIMER1))
+		 {
+			 pwm_set_duty_cycle(BL,0xFF);
+			 timer_start(TIMER1);
+		 }
+		 else if (timer_elapsed_ms(TIMER1) > 250)
+		 {
+			 pwm_set_duty_cycle(BL,0x00);
+		 }
+		 else if (timer_elapsed_ms(TIMER1) > 500)
+		 {
+			 pwm_set_duty_cycle(BL,0xFF);
+			 timer_stop(TIMER1);
+		 }
+		 blink_decrement--;
+	 }
+	 else
+	 {
+		 rgbled_turn_off(LED_RED);
+		 BLINK_STATE = NONE;
+		 pwm_set_duty_cycle(BL,0x00);
+	 }
+ }
+ 
+ void blink_right()
+ {
+	 if (blink_decrement)
+	 {
+		 if (!timer_is_running(TIMER1))
+		 {
+			 pwm_set_duty_cycle(BR,0xFF);
+			 timer_start(TIMER1);
+		 }
+		 else if (timer_elapsed_ms(TIMER1) > blink_period)
+		 {
+			 pwm_set_duty_cycle(BR,0x00);
+		 }
+		 else if (timer_elapsed_ms(TIMER1) > (2*blink_period))
+		 {
+			 pwm_set_duty_cycle(BR,0xFF);
+			 timer_stop(TIMER1);
+		 }
+		 blink_decrement--;
+	 }
+	 else
+	 {
+		 BLINK_STATE = NONE;
+		 pwm_set_duty_cycle(BR,0x00);
+	 }
+ }
+ 
+ void hazards()
+ {
+	 if (!timer_is_running(TIMER1))
+	 {
+		 pwm_set_duty_cycle(BR,0xFF);
+		 pwm_set_duty_cycle(BL,0xFF);
+		 timer_start(TIMER1);
+	 }
+	 else if (timer_elapsed_ms(TIMER1) > blink_period)
+	 {
+		 pwm_set_duty_cycle(BR,0x00);
+		 pwm_set_duty_cycle(BL,0x00);
+	 }
+	 else if (timer_elapsed_ms(TIMER1) > (2*blink_period))
+	 {
+		 pwm_set_duty_cycle(BR,0xFF);
+		 pwm_set_duty_cycle(BL,0xFF);
+		 timer_stop(TIMER1);
+	 }
+ }
